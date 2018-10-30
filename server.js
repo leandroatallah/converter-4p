8ce7282e9f89303b006698b13b323296f44a578f
Leandro Atallah <leandro.atallah@artedigitalinternet.com.br>
2018-10-30 17:27:48 -0300

    server.js atualização

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
-app = express()
-app.use(serveStatic(__dirname + "/dist"))
-var port = process.env.PORT || 5000
+const express = require('express')
+const path = require('path')
+const serveStatic = require('serve-static')
+const app = express()
+
+app.use(express.static(__dirname + "/dist/"))
+const port = process.env.PORT || 5000
 app.listen(port)
+
 console.log('server started '+ port)
